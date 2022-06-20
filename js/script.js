40b31c73219cc4f1056fbbf1afd161655ff0d701
Diego Medeiros <69081730+DiMedeiros15@users.noreply.github.com>
2022-06-19 23:54:59 -0300

    atualizações no formulario
    formulario funcionando !!

--- js/script.js
+++ js/script.js
@@ -1,116 +1,134 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        // sticky navbar on scroll script
-        if(this.scrollY > 20){
-            $('.navbar').addClass("sticky");
-        }else{
-            $('.navbar').removeClass("sticky");
-        }
-        
-        // scroll-up button show/hide script
-        if(this.scrollY > 500){
-            $('.scroll-up-btn').addClass("show");
-        }else{
-            $('.scroll-up-btn').removeClass("show");
-        }
-    });
+$(document).ready(function () {
+  $(window).scroll(function () {
+    // sticky navbar on scroll script
+    if (this.scrollY > 20) {
+      $(".navbar").addClass("sticky");
+    } else {
+      $(".navbar").removeClass("sticky");
+    }
 
-    // slide-up script
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
-        // removing smooth scroll on slide-up button click
-        $('html').css("scrollBehavior", "auto");
-    });
+    // scroll-up button show/hide script
+    if (this.scrollY > 500) {
+      $(".scroll-up-btn").addClass("show");
+    } else {
+      $(".scroll-up-btn").removeClass("show");
+    }
+  });
 
-    $('.navbar .menu li a').click(function(){
-        // applying again smooth scroll on menu items click
-        $('html').css("scrollBehavior", "smooth");
-    });
+  // slide-up script
+  $(".scroll-up-btn").click(function () {
+    $("html").animate({ scrollTop: 0 });
+    // removing smooth scroll on slide-up button click
+    $("html").css("scrollBehavior", "auto");
+  });
 
-    // toggle menu/navbar script
-    $('.menu-btn').click(function(){
-        $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
-    });
+  $(".navbar .menu li a").click(function () {
+    // applying again smooth scroll on menu items click
+    $("html").css("scrollBehavior", "smooth");
+  });
 
-    // animation script
-    var typed = new Typed (".escrevendo",{
-        strings: ["Desenvolvedor Web","Suporte"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
+  // toggle menu/navbar script
+  $(".menu-btn").click(function () {
+    $(".navbar .menu").toggleClass("active");
+    $(".menu-btn i").toggleClass("active");
+  });
 
-    var typed = new Typed (".escrevendo-2",{
-        strings: ["Desenvolvedor Web","Suporte"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
+  // animation script
+  var typed = new Typed(".escrevendo", {
+    strings: ["Desenvolvedor Web", "Suporte"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
 
-// Skills Hidden 
-    var arrow = document.querySelectorAll('.arrow_skills');
-    var close = document.querySelectorAll('.close');
+  var typed = new Typed(".escrevendo-2", {
+    strings: ["Desenvolvedor Web", "Suporte"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
 
-    arrow[0].addEventListener('click', function () {
-        
-        if (close[0].style.display === 'block') {
-            close[0].style.display = 'none';
-        } else {
-            close[0].style.display = 'block';
-        }
-    });
+  // Skills
+  var arrow = document.querySelectorAll(".arrow_skills");
+  var close = document.querySelectorAll(".close");
 
-    arrow[1].addEventListener('click', function () {
-        
-        if (close[1].style.display === 'block') {
-            close[1].style.display = 'none';
-        } else {
-            close[1].style.display = 'block';
-        }
-    });
+  arrow[0].addEventListener("click", function () {
+    if (close[0].style.display === "block") {
+      close[0].style.display = "none";
+    } else {
+      close[0].style.display = "block";
+    }
+  });
 
-    arrow[2].addEventListener('click', function () {
-        
-        if (close[2].style.display === 'block') {
-            close[2].style.display = 'none';
-        } else {
-            close[2].style.display = 'block';
-        }
-    });
-    var btn_arrow = document.querySelector('.btn-skills');
-    var close2 = document.querySelector('.close2');
+  arrow[1].addEventListener("click", function () {
+    if (close[1].style.display === "block") {
+      close[1].style.display = "none";
+    } else {
+      close[1].style.display = "block";
+    }
+  });
 
-    btn_arrow.addEventListener('click', function () {
-        
-        if (close2.style.display === 'block') {
-            close2.style.display = 'none';
-        } else {
-            close2.style.display = 'block';
-        }
-    });
+  arrow[2].addEventListener("click", function () {
+    if (close[2].style.display === "block") {
+      close[2].style.display = "none";
+    } else {
+      close[2].style.display = "block";
+    }
+  });
+  var btn_arrow = document.querySelector(".btn-skills");
+  var close2 = document.querySelector(".close2");
 
+  btn_arrow.addEventListener("click", function () {
+    if (close2.style.display === "block") {
+      close2.style.display = "none";
+    } else {
+      close2.style.display = "block";
+    }
+  });
 
-// owl carousel script
-    $('.carousel').owlCarousel({
-        margin: 20,
-        loop: true,
-        autoplay: true,
-        autoplayTimeOut: 2000,
-        autoplayHoverPause: true,
-        responsive: {
-            0:{
-                items: 1,
-                nav: false
-            },
-            600:{
-                items: 2,
-                nav: false
-            },
-            1000:{
-                items: 3,
-                nav: false
-            }
-        }
-    });
+  // carousel script
+  $(".carousel").owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplay: true,
+    autoplayTimeOut: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      },
+      600: {
+        items: 2,
+        nav: false,
+      },
+      1000: {
+        items: 3,
+        nav: false,
+      },
+    },
+  });
 });
+
+// let btn = document.getElementById("btn");
+// btn.addEventListener("click", function (e) {
+//   e.preventDefault();
+//   let nome = document.getElementById("nome").value;
+//   let email = document.getElementById("email").value;
+//   let assunto = document.getElementById("assunto").value;
+//   let mensagem = document.getElementById("mensagem").value;
+//   let corpo = ` nome: ${nome} 
+//                  email  ${email}
+//                  assunto: ${assunto} 
+//                  mensagem  ${mensagem}`;
+
+//   Email.send({
+//     Host: "smtp.yourisp.com",
+//     Username: "dimedeiros15@gmail.com",
+//     Password: "ljhyilaoccdtqtlc",
+//     To: "dimedeiros15@gmail.com",
+//     From: email,
+//     Subject: assunto,
+//     Body: corpo,
+//   }).then((message) => alert(message));
+// });
